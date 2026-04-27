35d460c737016251e3dac3dc474f0ed74e5174ed
fred-corp <7109145+fred-corp@users.noreply.github.com>
2026-04-27 08:41:20 +0000

    eeprom-sign 1.0.7

--- Formula/eeprom-sign.rb
+++ Formula/eeprom-sign.rb
@@ -11,19 +11,19 @@
 class EepromSign < Formula
   desc     "RSA-PSS sign, verify, and batch-flash Raspberry Pi HAT+ EEPROMs"
   homepage "https://github.com/fred-corp/eeprom-sign"
-  version  "1.0.4"
+  version  "1.0.7"
 
   on_macos do
-    url    "https://github.com/fred-corp/eeprom-sign/releases/download/v1.0.6/eeprom-sign-macos-arm64"
+    url    "https://github.com/fred-corp/eeprom-sign/releases/download/v1.0.7/eeprom-sign-macos-arm64"
     sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   end
   on_linux do
     if Hardware::CPU.arm?
       url    "https://github.com/fred-corp/eeprom-sign/releases/download/v1.0.6/eeprom-sign-linux-aarch64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "3acf2426b3d60655f51d59daa62f3290e8cc567bd5be02f6070f5af4bc6aad16"
     else
       url    "https://github.com/fred-corp/eeprom-sign/releases/download/v1.0.6/eeprom-sign-linux-x86_64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 ""
     end
   end
 
